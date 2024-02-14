9158b0ca8cdcc9b281975eae6f3a147fa1a2c00a
ValeriaGomez <3885805@alu.murciaeduca.es>
2024-02-14 12:48:54 +0100

    Modificación Manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "reuniones",
+    'name': "Gestión sala de reuniones",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -8,8 +8,8 @@
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Valeria Gómez",
+    'website': "https://www.valeriagomez.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
